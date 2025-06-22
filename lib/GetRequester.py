6e843ea7bacacb93eda2143f36b727fe88f5944d
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-06-22 08:30:54 +0300

    Complete GetRequester class with working GET and JSON handling

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,13 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Return as bytes to pass the test
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()  # Return as parsed Python list/dict
